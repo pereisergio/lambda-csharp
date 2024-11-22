52a0a1c6931581e5da3436c0b0c44d15d05e7091
Sergio Junior <pereisergio@outlook.com>
2024-11-22 14:02:55 -0300

    Commit Program

---
+++ Entities/Employee.cs
@@ -0,0 +1,16 @@
+namespace ExerciceSection15.Entities;
+
+public class Employee
+{
+
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(string name, string email, double salary)
+    {
+        Name = name;
+        Email = email;
+        Salary = salary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using ExerciceSection15.Entities;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace ExerciceSection15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"in.txt";
+
+            List<Employee> employees = new List<Employee>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        string email = line[1];
+                        double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+
+                        employees.Add(new Employee(name, email, salary));
+                    }
+                }
+
+                Console.Write("Enter salary: ");
+                double searchSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                var searchOne =
+                    from emp in employees
+                    where emp.Salary > searchSalary
+                    orderby emp.Name
+                    select emp.Email;
+
+                Console.WriteLine($"Email of people whose salary is more than {searchSalary.ToString("F2", CultureInfo.InvariantCulture)}:");
+                foreach (var email in searchOne)
+                {
+                    Console.WriteLine(email);
+                }
+
+                var searchTwo =
+                    from emp in employees
+                    where emp.Name[0] == 'M'
+                    select emp;
+
+                Console.WriteLine($"Sum of salary of people whose name starts with 'M': {searchTwo.Sum(emp => emp.Salary).ToString("F2", CultureInfo.InvariantCulture)}");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
